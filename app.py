b47ab58ceaf934ec0e31b28060bbacaae1cfe330
Lucas Luan <systemifp0909@gmail.com>
2026-04-21 20:03:18 -0300

    MOVIMENTA RIO

--- app.py
+++ app.py
@@ -2453,7 +2453,7 @@ def confirmacao():
     if not protocolo:
         return redirect(url_for("home"))
 
-    home_url = "https://educatech-criacao-de-marcas-e-logomarcas.onrender.com"
+    home_url = "https://movimenta-rio-guadalupe.onrender.com"
 
     return render_template_string(
         TEMPLATE_CONFIRMACAO,
